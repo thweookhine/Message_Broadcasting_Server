a406d5229f83e643d7444bc18e65e6113127986f
thweookhine <mathweookhine@gmail.com>
2025-04-16 12:17:18 +0630

    add docker-compose.yaml

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,9 @@ const {Sequelize} = require('sequelize')
 require('dotenv').config(); 
 
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-    host: 'localhost',
-    port: 5432, 
-    dialect: 'postgres'
+    host: process.env.DATABASE_HOST || 'postgres', 
+    port: 5432,
+    dialect: "postgres"
 })
 
 sequelize.authenticate()
